ebda7901e4707ac596e959373e51526d9e86c0d3
solangep1 <80253681+solangep1@users.noreply.github.com>
2022-05-29 22:41:42 +0200

    Add files via upload

---
+++ InputParser.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.util.Scanner;
+
+public class InputParser {
+
+    public Scanner sc= new Scanner(System.in);
+
+    public String scanHeroName(){
+        System.out.println("please choose your Hero: Hunter, Warrior, Mage, Healer");
+
+   /*if( sc.nextLine()!="Hunter" && sc.nextLine()!="Warrior"  && sc.nextLine()!="Mage" && sc.nextLine()!="Healer"){
+     System.out.println("Error");
+    }*/
+        return sc.nextLine();
+    }
+
+    public int scanHeroNumber(){
+        System.out.println("Please enter the number of heroes: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        return number;
+    }
+    public String scanFood(){
+        System.out.println("Enter name of food you wish to consume: ");
+
+        return sc.nextLine();
+    }
+
+    public String scanAction(){
+        System.out.println("Choose your action: Attack, Defend, Eat or Drink ");
+
+        return sc.nextLine();
+    }
+    public void closeScanner(){
+        sc.close();
+    }
+
+
+
+}
